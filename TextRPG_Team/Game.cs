ccb65ec5ab187d3cca1c947dd556805b747dffe4
Eunseok <79277772+Eunseok@users.noreply.github.com>
2025-02-06 20:06:30 +0900

    기본 씬들 생성

--- TextRPG_Team/Game.cs
+++ TextRPG_Team/Game.cs
@@ -7,10 +7,31 @@ public partial class Game
     public Game()
     {
     }
-    
+
     //전체적인 게임로직
     public void Run()
     {
     }
+
+    public void MainScene()
+    {
+
+    }
+
+    public void ShopScene()
+    {
+    }
+    
+
+    public void StatusScene()
+    {
+
+    }
+
+    public void BattleScene()
+    {
+
+    }
+
 }
 

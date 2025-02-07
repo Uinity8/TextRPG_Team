06f61f673aaa1d2da1b1029c8e42b01584e1b783
Kimahyeon <kay877@naver.com>
2025-02-07 11:07:30 +0900

    Create EnemyManager.cs
    각 오브젝트들 관리하려면 Manager가 필요할거 같아서 일단 만들어 봤어요.

---
+++ TextRPG_Team/Manager/EnemyManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using TextRPG_Team.Objects;
+
+namespace TextRPG_Team.Manager
+{
+    internal class EnemyManager
+    {
+        private List<Enemy> enemy = new List<Enemy>();  // 적 리스트
+        private Random random = new Random();
+
+        private EnemyManager() 
+        {
+            enemy.Add(new Enemy(2, "슬라임", 20, 5));
+            enemy.Add(new Enemy(5, "폭탄병", 30, 15));
+        }
+
+        public Enemy GetRandomEnemy()
+        {
+            int index = random.Next(enemy.Count); // 0 ~ (적군 개수 - 1) 사이의 랜덤 값
+            return enemy[index];
+        }
+    }
+}

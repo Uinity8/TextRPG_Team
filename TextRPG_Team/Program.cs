e4c9eef14983586d8001f0cf3d5897a3d796ec31
Eunseok <79277772+Eunseok@users.noreply.github.com>
2025-02-10 17:21:41 +0900

    undo

--- TextRPG_Team/Program.cs
+++ TextRPG_Team/Program.cs
@@ -5,9 +5,6 @@ class Program
     static void Main(string[] args)
     {
         GameState gameState = new GameState(); //인스턴스 생성
-        //임시코드
-        Item item = new Item("아이템", ItemType.Armor, 10, "아이템입니다.", 0);
-        gameState.Player.BuyItem(item);
         
         var initialScene = new MainScene(gameState); // 첫 번째 씬 설정
 

0bbf8190d643f7ed1c0ea44c313b24819808a089
Eunseok <79277772+Eunseok@users.noreply.github.com>
2025-02-10 16:42:24 +0900

    캐릭터 병합 로직에 Lv 합산 추가 및 Player 객체 초기화 코드 추가

--- TextRPG_Team/GameState.cs
+++ TextRPG_Team/GameState.cs
@@ -1,5 +1,7 @@
 //using TextRPG_Mockup.Objects;
 
+using TextRPG_Team.Objects;
+
 namespace TextRPG_Team;
 
 public class GameState
@@ -7,4 +9,5 @@ public class GameState
     //씬전환간에 필요한 인스터스들
     
     //Objects
+    public readonly Player Player = new Player("Chad", new Stats(100, 10, 5), 1500);
 }
